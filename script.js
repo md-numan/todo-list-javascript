165cad0f8356cd40162a5254977480a789605a91
numan <mdnumanking3@gmail.com>
2024-10-09 22:46:23 +0600

    todo

---
+++ script.js
@@ -0,0 +1,40 @@
+
+ const inputBox = document.getElementById("input_box");
+ const listContainer = document.getElementById("list_container");
+
+function addTask(){
+   if( inputBox.value === ' '){
+    alert("please enter a task name")
+   }else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+     li.appendChild(span)
+   }
+
+   inputBox.value = " ";
+   saveData()
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("check_list")
+        saveData()
+    }else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+
+function saveData(data){
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showData();
+
